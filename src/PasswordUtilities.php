0a1642f43ec578573eda0efa6be628b4113c951e
Adrian Suter <adrian@suter-wirz.ch>
2020-11-17 12:15:51 +0100

    Add password utilities

---
+++ src/PasswordUtilities.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdrianSuter\CryptoSuite;
+
+use Exception;
+use ParagonIE\Halite\Alerts\HaliteAlert;
+use ParagonIE\Halite\Symmetric\EncryptionKey;
+use ParagonIE\HiddenString\HiddenString;
+
+use function mb_strlen;
+use function random_int;
+
+class PasswordUtilities
+{
+    /**
+     * @var KeyFactory
+     */
+    protected $keyFactory;
+
+    /**
+     * @param KeyFactory $keyFactory
+     */
+    public function __construct(KeyFactory $keyFactory)
+    {
+        $this->keyFactory = $keyFactory;
+    }
+
+    /**
+     * @param HiddenString $password
+     * @param string       $salt
+     *
+     * @return EncryptionKey
+     * @throws HaliteAlert
+     */
+    public function derivePasswordKey(HiddenString $password, string $salt): EncryptionKey
+    {
+        return $this->keyFactory->derivePepperedEncryptionKey($password, $salt);
+    }
+
+    /**
+     * @param int    $length
+     * @param string $keyspace
+     *
+     * @return HiddenString
+     * @throws Exception
+     */
+    public function generatePassword(
+        int $length,
+        string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    ): HiddenString {
+        $str = '';
+        $max = mb_strlen($keyspace, 'UTF-8') - 1;
+
+        if ($max < 1) {
+            throw new Exception('$keyspace must be at least two characters long');
+        }
+
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= $keyspace[random_int(0, $max)];
+        }
+
+        return new HiddenString($str);
+    }
+}
